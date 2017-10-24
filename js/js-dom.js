afaacc009416f7c2199fbd2fcbaaa4112b5cb84b
DezkaReid <elmaildeldezkareid@gmail.com>
2017-10-24 15:43:02 -0500

    Evento click agregado al botón y al container usando bubbling

--- js/js-dom.js
+++ js/js-dom.js
@@ -6,6 +6,19 @@ function init() {
 	boton.textContent = "Soy un botón agregado con JS";
 	boton.classList.add("fancy-button");
 
+	var clickBoton = function (event) {
+		alert("Le diste click al botón");
+		console.log("Le diste click al botón", event.target);	
+	};
+
+	var clickContainer = function (event) {
+		alert("Le diste click al container");
+		console.log("Le diste click al container", event.target);	
+	};
+
+	boton.addEventListener('click', clickBoton);
+	container.addEventListener('click', clickContainer);
+
 	container.appendChild(boton);
 }
 
