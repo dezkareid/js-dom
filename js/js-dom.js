29fdc0020f59ad3f26df9a970b8276b249b54fce
DezkaReid <elmaildeldezkareid@gmail.com>
2017-10-24 03:25:00 -0500

    Agregando un elemento dinámicamente usando javascript

--- js/js-dom.js
+++ js/js-dom.js
@@ -1,6 +1,11 @@
 function init() {
 	var container = document.querySelector("#container");
 	container.style = "background-color: #2196F3;height: 10rem;";
+
+	var boton = document.createElement('button');
+	boton.textContent = "Soy un botón agregado con JS";
+
+	container.appendChild(boton);
 }
 
 window.addEventListener('load', init);
