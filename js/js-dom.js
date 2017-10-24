52f0d8017934f00edba2c7d1afc89f41b79e89c3
DezkaReid <elmaildeldezkareid@gmail.com>
2017-10-24 03:17:27 -0500

    Selección y modificación de un elemento, modificación de atributo style

--- js/js-dom.js
+++ js/js-dom.js
@@ -1,6 +1,6 @@
 function init() {
 	var container = document.querySelector("#container");
-	console.log(container);
+	container.style = "background-color: #2196F3;height: 10rem;";
 }
 
 window.addEventListener('load', init);
